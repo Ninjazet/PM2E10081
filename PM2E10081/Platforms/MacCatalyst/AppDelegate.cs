5e9953daf1490133b653ffa9f612e4bf40d79dc9
Ninjazet <castellanostejada2001@gmail.com>
2024-02-16 22:21:51 -0600

    w

---
+++ PM2E10081/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PM2E10081
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
